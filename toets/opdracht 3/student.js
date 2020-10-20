554a30592613e3fbbae7ada85dc591cc5d6c64c4
bobvberckel <29198@ma-web.nl>
2020-10-20 10:44:50 +0200

    Update

--- toets/opdracht 3/student.js
+++ toets/opdracht 3/student.js
@@ -1,12 +1,44 @@
-function Student(naam, studentId, klas) {
+function Student(naam, studentId) {
     let obj = this;
     this.name = naam + 'Hi';
     this.id = studentId;
     obj.cijfers = [];
-    let studentklas = klas;
+    this.klas = '';
+
+    this.pasKlasAan = function(klas) {
+        this.klas = klas;
+    }
+
+    this.pasNaamAan = function(newName) {
+        this.naam = newName;
+    }
+
+    this.veranderId = function(newID) {
+        this.id = newID;
+    }
+
+    this.veranderCijfers = function(newArray) {
+        this.cijfers = newArray;
+    }
+
+    this.geefNaam = function() {
+        return this.name;
+    }
+
+    this.geefId = function() {
+        return this.id;
+    }
+
+    this.geefCijfers = function() {
+        return this.cijfers;
+    }
+
+    this.geefklas = function() {
+        return this.klas;
+    }
 }
 
-let student = new Student('myName', '0001', `MD2aa`);
+let student = new Student('myName', '0001');
 
 // Hier moet je de variabele klas een waarde geven bijvoorbeeld MD2
-console.log(student.studentklas)
+console.log(student.geefklas);
